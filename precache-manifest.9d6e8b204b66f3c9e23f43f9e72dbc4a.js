844ec85082897d6cc4459797b405c1f09dfdc125
Crow <devcrowmaster@gmail.com>
2020-05-19 14:51:13 -0500

    Updates

---
+++ precache-manifest.9d6e8b204b66f3c9e23f43f9e72dbc4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe71901dce06011a3f26f46ee9d4d67",
+    "url": "/insurance-quotation/index.html"
+  },
+  {
+    "revision": "e36ff245efd9d21700ec",
+    "url": "/insurance-quotation/static/css/main.92263ba1.chunk.css"
+  },
+  {
+    "revision": "d509e39d278cacc3a302",
+    "url": "/insurance-quotation/static/js/2.dd65279a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/insurance-quotation/static/js/2.dd65279a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36ff245efd9d21700ec",
+    "url": "/insurance-quotation/static/js/main.dd28e1a9.chunk.js"
+  },
+  {
+    "revision": "f6625700af2fabb4d5f6",
+    "url": "/insurance-quotation/static/js/runtime-main.ace3367f.js"
+  }
+]);
